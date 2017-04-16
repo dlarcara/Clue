faca0796699ae0df03c01dff3e9bbbd74b085ff3
Unknown <dlarcara11@gmail.com>
2017-04-15 20:50:32 -0400

    Continued work on setup page

--- src/pages/setup/setup.page.ts
+++ src/pages/setup/setup.page.ts
@@ -13,6 +13,7 @@ import * as _ from 'lodash';
 })
 
 export class SetupPage {
+    detective: any;
     players: any[];
     setupStep: number;
 
@@ -29,8 +30,9 @@ export class SetupPage {
 
         //Default template for players
         this.players = _.map(this.allCardsByCategory[CardCategory.SUSPECT].cards, (suspect, index) => {
-            return { isDetective: index == 0, name: '', suspect: suspect, isPlaying: index < 3, extraCard: false }
+            return { name: '', suspect: suspect, isPlaying: index < 3, extraCard: false }
         });
+        this.detective = this.players[0]; //Default the detective to first player
     }
 
     getPlayersToDisplayBasedOnSetupStep(setupStep) : any[]
@@ -38,7 +40,27 @@ export class SetupPage {
         return _.filter(this.players, (player) => setupStep == 1 ? true : player.isPlaying);
     }
 
+    getPlayingPlayers() : any[]
+    {
+        return _.filter(this.players, 'isPlaying');
+    }
+
     reorderItems(indexes) : void {
         this.players = reorderArray(this.players, indexes);
+    }
+
+    playerIsDetective(player) : Boolean
+    {
+        return _.isEqual(player, this.detective);
+    }
+
+    isPlayerCountValid() : Boolean
+    {
+        return this.getPlayingPlayers().length >= 3;
+    }
+
+    arePlayerNamesValid() : Boolean
+    {
+        return _.every(this.getPlayingPlayers(), 'name');
     }
 }
