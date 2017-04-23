20cdad3d3d8e8c6b2dc4203c2b2dae4fc5c0e911
Unknown <dlarcara11@gmail.com>
2017-04-22 22:36:50 -0400

    Fixed enum values on GameCardService

--- src/app/shared/game-cards/game-card.service.ts
+++ src/app/shared/game-cards/game-card.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { CardCategory } from '../../game/index';
+import { CardCategory, Card } from '../../game/index';
 import { GameCard } from './game-card.model';
 
 import * as _ from 'lodash';
@@ -15,27 +15,27 @@ export class GameCardService
     private readonly roomIconLocation = `${this.baseIconLocation}/rooms`;
 
     readonly ALLCARDS = [
-        new GameCard(CardCategory.SUSPECT, 0, 'Mr. Green', `${this.suspectIconLocation}/green.png`),
-        new GameCard(CardCategory.SUSPECT, 1, 'Colonel Mustard', `${this.suspectIconLocation}/mustard.png`),
-        new GameCard(CardCategory.SUSPECT, 2, 'Mrs. Peacock', `${this.suspectIconLocation}/peacock.png`),
+        new GameCard(CardCategory.SUSPECT, 5, 'Mr. Green', `${this.suspectIconLocation}/green.png`),
+        new GameCard(CardCategory.SUSPECT, 4, 'Colonel Mustard', `${this.suspectIconLocation}/mustard.png`),
+        new GameCard(CardCategory.SUSPECT, 0, 'Mrs. Peacock', `${this.suspectIconLocation}/peacock.png`),
         new GameCard(CardCategory.SUSPECT, 3, 'Professor Plum', `${this.suspectIconLocation}/plum.png`),
-        new GameCard(CardCategory.SUSPECT, 4, 'Mrs. Scarlet', `${this.suspectIconLocation}/scarlet.png`),
-        new GameCard(CardCategory.SUSPECT, 5, 'Mrs. White', `${this.suspectIconLocation}/white.png`),
-        new GameCard(CardCategory.WEAPON, 0, 'Candlestick', `${this.weaponIconLocation}/candlestick.png`),
-        new GameCard(CardCategory.WEAPON, 1, 'Knife', `${this.weaponIconLocation}/knife.png`),
-        new GameCard(CardCategory.WEAPON, 2, 'Lead Pipe', `${this.weaponIconLocation}/leadpipe.png`),
-        new GameCard(CardCategory.WEAPON, 3, 'Revolver', `${this.weaponIconLocation}/revolver.png`),
-        new GameCard(CardCategory.WEAPON, 4, 'Rope', `${this.weaponIconLocation}/rope.png`),
-        new GameCard(CardCategory.WEAPON, 5, 'Wrench', `${this.weaponIconLocation}/wrench.png`),
-        new GameCard(CardCategory.ROOM, 0, 'Ballroom', `${this.roomIconLocation}/ballroom.png`),
-        new GameCard(CardCategory.ROOM, 1, 'Billiard Room', `${this.roomIconLocation}/billiardroom.png`),
-        new GameCard(CardCategory.ROOM, 2, 'Conservatory', `${this.roomIconLocation}/conservatory.png`),
-        new GameCard(CardCategory.ROOM, 3, 'Dining Room', `${this.roomIconLocation}/diningroom.png`),
-        new GameCard(CardCategory.ROOM, 5, 'Hall', `${this.roomIconLocation}/hall.png`),
-        new GameCard(CardCategory.ROOM, 6, 'Kitchen', `${this.roomIconLocation}/kitchen.png`),
-        new GameCard(CardCategory.ROOM, 7, 'Library', `${this.roomIconLocation}/library.png`),
-        new GameCard(CardCategory.ROOM, 8, 'Lounge', `${this.roomIconLocation}/lounge.png`),
-        new GameCard(CardCategory.ROOM, 9, 'Stucy', `${this.roomIconLocation}/study.png`)
+        new GameCard(CardCategory.SUSPECT, 1, 'Mrs. Scarlet', `${this.suspectIconLocation}/scarlet.png`),
+        new GameCard(CardCategory.SUSPECT, 2, 'Mrs. White', `${this.suspectIconLocation}/white.png`),
+        new GameCard(CardCategory.WEAPON, 2, 'Candlestick', `${this.weaponIconLocation}/candlestick.png`),
+        new GameCard(CardCategory.WEAPON, 5, 'Knife', `${this.weaponIconLocation}/knife.png`),
+        new GameCard(CardCategory.WEAPON, 4, 'Lead Pipe', `${this.weaponIconLocation}/leadpipe.png`),
+        new GameCard(CardCategory.WEAPON, 0, 'Revolver', `${this.weaponIconLocation}/revolver.png`),
+        new GameCard(CardCategory.WEAPON, 1, 'Rope', `${this.weaponIconLocation}/rope.png`),
+        new GameCard(CardCategory.WEAPON, 3, 'Wrench', `${this.weaponIconLocation}/wrench.png`),
+        new GameCard(CardCategory.ROOM, 6, 'Ballroom', `${this.roomIconLocation}/ballroom.png`),
+        new GameCard(CardCategory.ROOM, 8, 'Billiard Room', `${this.roomIconLocation}/billiardroom.png`),
+        new GameCard(CardCategory.ROOM, 3, 'Conservatory', `${this.roomIconLocation}/conservatory.png`),
+        new GameCard(CardCategory.ROOM, 0, 'Dining Room', `${this.roomIconLocation}/diningroom.png`),
+        new GameCard(CardCategory.ROOM, 2, 'Hall', `${this.roomIconLocation}/hall.png`),
+        new GameCard(CardCategory.ROOM, 1, 'Kitchen', `${this.roomIconLocation}/kitchen.png`),
+        new GameCard(CardCategory.ROOM, 4, 'Library', `${this.roomIconLocation}/library.png`),
+        new GameCard(CardCategory.ROOM, 5, 'Lounge', `${this.roomIconLocation}/lounge.png`),
+        new GameCard(CardCategory.ROOM, 7, 'Study', `${this.roomIconLocation}/study.png`)
     ];
 
     getAllCards() : GameCard[]
@@ -46,6 +46,16 @@ export class GameCardService
     getCardsByCategory(cardCategory : CardCategory) : GameCard[]
     {
         return _.filter(this.ALLCARDS, (c) => { return c.cardCategory == cardCategory; });
+    }
+
+    getCard(cardCategory : CardCategory, cardIndex : number) : GameCard
+    {
+        return _.find(this.ALLCARDS, (c) => { return c.cardCategory == cardCategory && c.cardIndex == cardIndex; });
+    }
+
+    convertToCard(cardCategory : CardCategory, cardIndex : number) : Card
+    {
+        return new Card(cardCategory, cardIndex);
     }
 
     groupAllCardsByCategory() : any[]
