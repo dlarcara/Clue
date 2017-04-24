87de5ebd6a71682e5fc2847602900be0d68bf6cd
Unknown <dlarcara11@gmail.com>
2017-04-23 21:35:38 -0400

    Temp player list icon template

--- src/app/shared/player-icon/player-icon.component.ts
+++ src/app/shared/player-icon/player-icon.component.ts
@@ -4,7 +4,9 @@ import { Player } from '../../game/index';
 
 @Component({
     selector: 'player-icon',
-    template: '{{player.name.substring(0,1)}}'
+    template: `<span [style.color]=suspectBgColors[player.suspect]>
+        {{player.name.substring(0,1)}}
+    </span>`
     // template: `<ng2-letter-avatar avatar-data='{{player.name}}' 
     //                               avatar-height='25px' 
     //                               avatar-width='25px' 
@@ -16,7 +18,7 @@ import { Player } from '../../game/index';
 })
 
 export class PlayerIconComponent {
-    private suspectBgColors : string[] = ['#253783','#B22E2D','#000000','#522D61','#FDCD26','#1A4C28']
+    private suspectBgColors : string[] = ['#253783','#B22E2D','#000000','#522D61','#DFBD45','#1A4C28']
     
     @Input() player: Player
 }
