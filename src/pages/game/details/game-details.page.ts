2c0808e2ba81274d92a95779a34a00485bcc3d08
Unknown <dlarcara11@gmail.com>
2017-05-01 00:43:28 -0400

    Add filtering to all turns, prep for adding resolutions.

--- src/pages/game/details/game-details.page.ts
+++ src/pages/game/details/game-details.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
 
-import { GameTracker, CardCategory, Turn, Player, Card } from '../../../app/game/index';
+import { GameTracker, CardCategory, Turn, Player, Card, Suspect, Weapon, Room } from '../../../app/game/index';
 import { GameCardService } from '../../../app/shared/index';
 
 import * as _ from "lodash";
@@ -15,17 +15,61 @@ import * as _ from "lodash";
 export class GameDetailsPage
 {
     gameTracker: GameTracker
+    
+    showFilters: Boolean
+    filterDoNotShowPasses: Boolean
+    filterShowOnlyUnresolvedGuesses: Boolean
+    filterPlayer: Player
+    filterSuspect: Card
+    filterWeapon: Card
+    filterRoom: Card
+
+    suspectCards: Card[]
+    weaponCards: Card[]
+    roomCards: Card[]
 
     CardCategory = CardCategory;
 
     constructor(private navParams : NavParams, private gameCardService : GameCardService)
     {
+        //Set Default Filter Values;
+        this.showFilters = false;
+        this.filterDoNotShowPasses = false;
+        this.filterShowOnlyUnresolvedGuesses = false;
+        this.filterPlayer = null;
+        this.filterSuspect = null;
+        this.filterWeapon = null;
+        this.filterRoom = null;
+
         this.gameTracker = navParams.get('gameTracker');
+
+        this.suspectCards = gameCardService.getCardsByCategory(CardCategory.SUSPECT);
+        this.weaponCards = gameCardService.getCardsByCategory(CardCategory.WEAPON);
+        this.roomCards = gameCardService.getCardsByCategory(CardCategory.ROOM);
     }
 
     getTurns() : Turn[]
     {
-        return _.orderBy(this.gameTracker.turns, 'number', 'desc');
+        let filteredTurns = _.filter(this.gameTracker.turns, (t : Turn) => {
+            if (this.filterDoNotShowPasses && !t.guess)
+                return false;
+                
+            if (this.filterPlayer && !_.isEqual(t.player, this.filterPlayer))
+                return false;
+
+            if (this.filterSuspect != null && !(t.guess && _.isEqual(t.guess.suspect, this.filterSuspect.cardIndex)))
+                return false;
+
+            if (this.filterWeapon != null && !(t.guess && _.isEqual(t.guess.weapon, this.filterWeapon.cardIndex)))
+                return false;
+
+            if (this.filterRoom != null && !(t.guess && _.isEqual(t.guess.room, this.filterRoom.cardIndex)))
+                return false;
+
+            return true;
+        });
+
+        return _.orderBy(filteredTurns, 'number', 'desc');
     }
 
     getCardDisplay(cardCategory : CardCategory, cardIndex : number) : string
