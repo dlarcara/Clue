e1d18f6cad47d5d9126bb94a1eed893d4f20bc0f
Unknown <dlarcara11@gmail.com>
2017-05-01 21:45:24 -0400

    Add guess resolution to game details page

--- src/pages/game/details/game-details.page.ts
+++ src/pages/game/details/game-details.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { NavParams } from 'ionic-angular';
 
-import { GameTracker, CardCategory, Turn, Player, Card } from '../../../app/game/index';
+import { GameTracker, CardCategory, Turn, Player, Card, Guess, CellStatus } from '../../../app/game/index';
 import { GameCardService } from '../../../app/shared/index';
 
 import * as _ from "lodash";
@@ -18,7 +18,7 @@ export class GameDetailsPage
     
     showFilters: Boolean
     filterDoNotShowPasses: Boolean
-    filterShowOnlyUnresolvedGuesses: Boolean
+    filterShowOnlyUnresolvedTurns: Boolean
     filterPlayer: Player
 
     suspectCards: Card[]
@@ -26,13 +26,14 @@ export class GameDetailsPage
     roomCards: Card[]
 
     CardCategory = CardCategory;
+    CellStatus = CellStatus;
 
     constructor(private navParams : NavParams, private gameCardService : GameCardService)
     {
         //Set Default Filter Values;
         this.showFilters = false;
         this.filterDoNotShowPasses = false;
-        this.filterShowOnlyUnresolvedGuesses = false;
+        this.filterShowOnlyUnresolvedTurns = false;
         this.filterPlayer = null;
 
         this.gameTracker = navParams.get('gameTracker');
@@ -50,6 +51,18 @@ export class GameDetailsPage
                 
             if (this.filterPlayer && !_.isEqual(t.player, this.filterPlayer))
                 return false;
+
+            if (this.filterShowOnlyUnresolvedTurns)
+            {
+                if (!t.guess)
+                    return false
+                
+                if (this.playerIsDetective(t.player) || this.playerIsDetective(t.guess.playerThatShowed))
+                    return false;
+
+                if (t.guess.resolvedTurn)
+                    return false;
+            }
 
             return true;
         });
@@ -70,5 +83,32 @@ export class GameDetailsPage
     playerIsDetective(player : Player) : Boolean
     {
         return _.isEqual(player, this.gameTracker.getDetective())
+    }
+
+    shouldShowTurnResolution(turn : Turn) : Boolean
+    {
+        if (!turn.guess)
+            return false;
+
+        if (this.playerIsDetective(turn.player) || this.playerIsDetective(turn.guess.playerThatShowed))
+            return false;
+
+        return true;
+    }
+
+    getCardByCategory(guess : Guess, cardCategory : CardCategory) : Card
+    {
+        switch(cardCategory)
+        {
+            case CardCategory.SUSPECT: return new Card(CardCategory.SUSPECT, guess.suspect);
+            case CardCategory.WEAPON: return new Card(CardCategory.WEAPON, guess.weapon);
+            case CardCategory.ROOM: return new Card(CardCategory.ROOM, guess.room);
+        }
+    }
+
+    getStatusForCard(guess : Guess, cardCategory : CardCategory) : CellStatus
+    {
+        let card = this.getCardByCategory(guess, cardCategory);
+        return this.gameTracker.getStatusForPlayerAndCard(guess.playerThatShowed, card);
     }
 }
