4460894db41374899c27eb13d880d53198b2b924
Unknown <dlarcara11@gmail.com>
2017-05-02 20:19:45 -0400

    Improved usage for game sheet page

--- src/pages/game/sheet/game-sheet.page.ts
+++ src/pages/game/sheet/game-sheet.page.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 
-import { NavParams, AlertController } from 'ionic-angular';
+import { NavParams, AlertController, Content } from 'ionic-angular';
 
 import { GameCardService } from '../../../app/shared/index';
-import { GameTracker, Player, Card, CardCategory, CellStatus, Verdict } from '../../../app/game/index';
+import { GameTracker, Player, Card, CardCategory, CellStatus, Verdict, GameSheet } from '../../../app/game/index';
 
 import * as _ from "lodash";
 
@@ -18,6 +18,11 @@ export class GameSheetPage {
     readonly roomCards: Card[]
 
     gameTracker : GameTracker
+    displayedTurn : number
+    showTurnSlide : Boolean
+    showVerdict : Boolean
+
+    @ViewChild(Content) content: Content;
 
     constructor(private navParams : NavParams, private gameCardService : GameCardService, private alertCtrl: AlertController) 
     {
@@ -26,11 +31,48 @@ export class GameSheetPage {
         this.roomCards = gameCardService.getCardsByCategory(CardCategory.ROOM);
 
         this.gameTracker = this.navParams.get('gameTracker');
+        this.resetFilters();
+    }
+
+    ionViewDidEnter() : void
+    {
+        this.resetFilters();
+    }
+
+    private resetFilters() : void
+    {
+        this.showTurnSlide = false;
+        this.showVerdict = true;
+        this.displayedTurn = (this.gameTracker.turns.length-1);
+    }
+
+    toggleVerdict() : void
+    {
+        this.showVerdict = !this.showVerdict;
+        this.content.resize();
+    }
+
+    toggleTurnSlide() : void 
+    {
+        this.showTurnSlide = !this.showTurnSlide;
+        this.content.resize();
+    }
+
+    getTurnColor() : string
+    {
+        if (this.displayedTurn == (this.gameTracker.turns.length - 1))
+            return 'valid';
+
+        if ((this.displayedTurn / this.gameTracker.turns.length) > .25)
+            return 'warning';
+
+        return 'invalid';
     }
 
     getCellClass(player : Player, card: Card) : string
     {
-        let cellStatus = this.gameTracker.getStatusForPlayerAndCard(player, card);
+        let gameSheet = this.getGameSheetForDisplayedTurn();
+        let cellStatus = gameSheet.getStatusForPlayerAndCard(player, card);
         
         if (cellStatus == CellStatus.HAD)
             return 'card-had';
@@ -43,13 +85,19 @@ export class GameSheetPage {
 
     getVerdict() : any
     {
-        let verdict = this.gameTracker.getVerdict();
+        let gameSheet = this.getGameSheetForDisplayedTurn();
+        let verdict = gameSheet.getVerdict();
         
         return {
             suspect: verdict ? this.gameCardService.getCard(CardCategory.SUSPECT, verdict.suspect) : null,
             weapon: verdict ? this.gameCardService.getCard(CardCategory.WEAPON, verdict.weapon) : null,
             room: verdict ? this.gameCardService.getCard(CardCategory.ROOM, verdict.room) : null,
         }
+    }
+
+    private getGameSheetForDisplayedTurn() : GameSheet
+    {
+        return this.gameTracker.turns[this.displayedTurn].resultingSheet;
     }
 
     showPlayerDetails(player) : void
@@ -81,7 +129,7 @@ export class GameSheetPage {
         alert.present();
     }
 
-    showVerdict(card : Card) : void
+    showVerdictAlert(card : Card) : void
     {
         let alert = this.alertCtrl.create({
             title: card.friendlyName,
