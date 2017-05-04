bd863caf928f6b2e143dd4f27f5fc0e11a0a6878
Unknown <dlarcara11@gmail.com>
2017-05-03 23:26:00 -0400

    Off by 1 error when tracking what guess a card was marked as had or not hard

--- src/app/game/game-algorithm.service.ts
+++ src/app/game/game-algorithm.service.ts
@@ -131,7 +131,7 @@ export class GameAlgorithm
             return;
 
         //Mark this card as had by this player
-        this._gameSheet.markCardAsHadByPlayer(player, card, this._turns.length + 1);
+        this._gameSheet.markCardAsHadByPlayer(player, card, this._turns.length);
 
         //Mark all other players as not having this card
         let allOtherPlayers = _.differenceWith(this._players, [player], _.isEqual);
@@ -150,7 +150,7 @@ export class GameAlgorithm
             return;
 
         //Mark this card as not had by a player
-        this._gameSheet.markCardAsNotHadByPlayer(player, card, this._turns.length + 1);
+        this._gameSheet.markCardAsNotHadByPlayer(player, card, this._turns.length);
 
         //Mark all remaining cards as had by this player if all their not had cards have been identified
         let knowNotHadCardsForPlayer = this._gameSheet.getAllCardsForPlayerInGivenStatus(player, CellStatus.NOTHAD);
