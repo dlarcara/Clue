d6241cf3b31212e23a06a7d996344020a067b84c
Unknown <dlarcara11@gmail.com>
2017-05-03 20:35:56 -0400

    Fix for restarting a game from saved details, need to wipe off resolved turns on guess, let algorithm take care of that.

--- src/app/shared/game-loader/game-loader.service.ts
+++ src/app/shared/game-loader/game-loader.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 
+import { GameTracker } from '../../game/index';
 import { GameDetails } from '../index';
 
 import * as _ from 'lodash';
@@ -19,6 +20,24 @@ export class GameLoaderService
             return null;
 
         return JSON.parse(localStorage.getItem('gameDetails'));
+    }
+
+    replayGame(gameDetails : GameDetails) : GameTracker
+    {
+        let gameTracker = new GameTracker(gameDetails.players, gameDetails.detectivesCards);
+        _.forEach(gameDetails.turns, (turn) => {
+            if (!turn.guess)
+                gameTracker.enterPass(turn.player);
+            else
+            {
+                //Wipe resolved Turn off guess, algorithm will recalculate this
+                turn.guess.resolvedTurn = null;
+
+                gameTracker.enterTurn(turn.guess);
+            }    
+        });
+
+        return gameTracker;
     }
 
     removeGame() : void
--- src/pages/game/game-tabs.page.ts
+++ src/pages/game/game-tabs.page.ts
@@ -4,7 +4,7 @@ import { NavParams } from 'ionic-angular';
 
 import { GameHomePage, GameSheetPage, GameDetailsPage, GameSettingsPage } from './index';
 
-import { GameDetails } from '../../app/shared/index';
+import { GameDetails, GameLoaderService } from '../../app/shared/index';
 import { GameTracker } from '../../app/game/index';
 
 import * as _ from "lodash";
@@ -23,7 +23,7 @@ export class GameTabsPage {
     tabParams: any;
     gameTracker: GameTracker
 
-    constructor(private navParams : NavParams) {
+    constructor(private navParams : NavParams, private gameLoader : GameLoaderService) {
         this.gameHome = GameHomePage;
         this.gameSheet = GameSheetPage;
         this.gameDetails = GameDetailsPage;
@@ -33,15 +33,7 @@ export class GameTabsPage {
         {
             //Bulid game from saved data
             let gameDetails : GameDetails = this.navParams.get('gameDetails');
-            let gameTracker = new GameTracker(gameDetails.players, gameDetails.detectivesCards);
-            _.forEach(gameDetails.turns, (turn) => {
-                if (!turn.guess)
-                    gameTracker.enterPass(turn.player);
-                else
-                    gameTracker.enterTurn(turn.guess);
-            });
-
-            this.gameTracker = gameTracker;
+            this.gameTracker = gameLoader.replayGame(gameDetails);
             this.tabParams = { gameTracker: this.gameTracker, activePlayer: gameDetails.activePlayer };
         }
         else
