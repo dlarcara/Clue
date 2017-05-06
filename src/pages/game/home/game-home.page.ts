42e6f7c00a58329fff015043fd48f6318abab2e0
Unknown <dlarcara11@gmail.com>
2017-05-06 01:06:56 -0400

    Add toast when new verdict information is found out.

--- src/pages/game/home/game-home.page.ts
+++ src/pages/game/home/game-home.page.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 
 import { ToastController  } from 'ionic-angular';
 
-import { GameTracker } from '../../../app/game/index';
+import { GameTracker, Verdict, Card, CardCategory } from '../../../app/game/index';
 import { GameCardService } from '../../../app/shared/index';
 import { GuessEntryComponent } from '../index';
 
@@ -21,7 +21,8 @@ export class GameHomePage {
     guessEntered(guess) : void
     {
         //Make sure guess is entered successfully before moving on to next player
-        let entrySuccessful : Boolean = true;       
+        let entrySuccessful : Boolean = true;      
+        let previousVerdict = this.gameTracker.getVerdict(); 
         try{
             if (!guess)
                 this.gameTracker.enterPass(this.gameTracker.getActivePlayer());
@@ -36,6 +37,9 @@ export class GameHomePage {
         
         if (entrySuccessful)
         {
+            if (!_.isEqual(previousVerdict, this.gameTracker.getVerdict()))
+                this.showVerdictIdentified(previousVerdict, this.gameTracker.getVerdict());
+                
             this.guessEntry.resetEntry();
             this.showGuessEntered(this.gameTracker.getActivePlayer(), guess);
         }
@@ -54,6 +58,38 @@ export class GameHomePage {
     private showGuessError(message : string)
     {
         let toast = this.toastCtrl.create({ message: message, duration: 5000 });
+        toast.present();
+    }
+
+    private showVerdictIdentified(previousVerdict : Verdict, currentVerdict : Verdict) : void
+    {
+        let verdictsLearned : Card[] = [];
+        let suspect = new Card(CardCategory.SUSPECT, currentVerdict.suspect);
+        if (previousVerdict.suspect == null && currentVerdict.suspect != null)
+            verdictsLearned.push(suspect)
+
+        let weapon = new Card(CardCategory.WEAPON, currentVerdict.weapon);
+        if (previousVerdict.weapon == null && currentVerdict.weapon != null)
+            verdictsLearned.push(weapon)
+
+        let room = new Card(CardCategory.ROOM, currentVerdict.room);
+        if (previousVerdict.room == null && currentVerdict.room != null)
+            verdictsLearned.push(room)
+
+        let fullSolutionKnown = (currentVerdict.suspect != null && currentVerdict.weapon != null && currentVerdict.room != null );
+        let message : string;
+        if (fullSolutionKnown)
+        {
+            message = `Full solution identified! ${suspect.friendlyName} in the ${room.friendlyName} with the ${weapon.friendlyName}!`;
+        }
+        else
+        {
+            debugger;
+            let newVerdcitsDisplay = verdictsLearned.map((c) => c.friendlyName.toUpperCase()).join(', ');
+            message = `Part of the solution identified! ${newVerdcitsDisplay}!`;
+        }
+            
+        let toast = this.toastCtrl.create({ position: 'middle', message: message, duration: 3000, cssClass: 'text-center' });
         toast.present();
     }
 }
