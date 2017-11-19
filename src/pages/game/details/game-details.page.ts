4fd9d0e476acd13e47e7f716fbdb07e088bf55a4
David Larcara <dlarcara11@gmail.com>
2017-11-19 10:07:54 -0500

    Bug fix for not treating a guess where no one showed as an open guess

--- src/pages/game/details/game-details.page.ts
+++ src/pages/game/details/game-details.page.ts
@@ -55,7 +55,7 @@ export class GameDetailsPage implements OnInit
 
             if (this.filterOptions.onlyShowOpenGuesses)
             {
-                if(!t.guess || t.player.isDetective || t.guess.playerThatShowed.isDetective || t.guess.resolvedTurn || t.number == 0)
+                if(!t.guess || t.player.isDetective || !t.guess.cardShown || t.guess.playerThatShowed.isDetective || t.guess.resolvedTurn || t.number == 0)
                     return false;
             }
 
